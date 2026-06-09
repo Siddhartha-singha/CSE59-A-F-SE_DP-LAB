b29b7a7530ef2cf4c4f7e8e3e08da30a6d59408a
Siddhartha Singha <158442777+Siddhartha-singha@users.noreply.github.com>
2026-06-09 11:19:19 +0600

    Update YouDo1.java

--- Task 2/YouDo1.java
+++ Task 2/YouDo1.java
@@ -1,1 +1,12 @@
 
+//1 Number
+public class ContestStats {
+     int[] scores = {85, 90, 95};
+     void printFirstScore() {
+         System.out.println("First Scores = "+scores[0]);
+     }
+     public static void main(String[] args) {
+         ContestStats stats = new ContestStats();
+         stats.printFirstScore();
+     }
+}
