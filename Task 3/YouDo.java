04c5231d3f39d4145d815d1a8e98f8119a45c1fe
Siddhartha Singha <158442777+Siddhartha-singha@users.noreply.github.com>
2026-06-16 10:15:09 +0600

    Update YouDo.java

--- Task 3/YouDo.java
+++ Task 3/YouDo.java
@@ -1,1 +1,39 @@
+//user info
+public class User {
+    private String username;
+    private String email;
 
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // create email validator
+
+    public static class EmailValidator {
+        public static boolean isValid(String email) {
+            return email != null && email.contains("@");
+        }
+    }
+
+    // create user repository
+
+    public static class UserRepository {
+        public void save(User user) {
+            if (EmailValidator.isValid(user.getEmail())) {
+                System.out.println("connecting to database...");
+                System.out.println("saving user " + user.getUsername() + " to the user table");
+            } else {
+                System.out.println("Invalid email format");
+            }
+        }
+    }
+}
