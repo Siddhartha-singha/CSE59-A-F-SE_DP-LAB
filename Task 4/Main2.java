f08ecb0fa50acdc44169a37b6786ef1b83b34b39
Siddhartha Singha <158442777+Siddhartha-singha@users.noreply.github.com>
2026-06-23 10:52:26 +0600

    Implement TeamMember hierarchy with Employee and Contractor

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
 
+        Employee emp = new Employee("Alice");
+        Contractor contractor = new Contractor("Bob");
+
+        emp.calculateBonus();
+        contractor.submitInvoice();
+    }
+}
+abstract class TeamMember {
+    protected String name;
+
+    public TeamMember(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+interface BonusEligible{
+    void calculateBonus();
+}
+class Employee extends TeamMember implements BonusEligible{
+    public Employee(String name){
+        super(name);
+    }
+    @Override
+    public void calculateBonus(){
+        System.out.println("Calculating standard employee bonus ..");
+    }
+}
+class Contractor extends TeamMember {
+    public Contractor(String name){
+        super(name);
+    }
+    public void submitInvoice(){
+        System.out.println(" Submitting contractor invoice ..");
+    }
+}
